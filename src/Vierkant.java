c42e8f64bddcc609beb9515244f06b8c6ecee3b6
adriaanboon <71772080+adriaanboon@users.noreply.github.com>
2020-10-19 19:50:31 +0200

    1 tot en met 100 in een vierkant uitgeprint.

--- src/Vierkant.java
+++ src/Vierkant.java
@@ -1,2 +1,24 @@
 public class Vierkant {
+    public static void main(String[] args) {
+
+        int getal;
+        int nwregel;
+        int startrij;
+
+        for (getal = 1; getal <= 9; getal++) {
+            System.out.print(" 0");
+            System.out.print(getal);
+        }
+        System.out.print(" 10");
+
+        for (nwregel=1; nwregel<10; nwregel++) {
+            startrij = nwregel * 10+1;
+            System.out.println();
+            for (getal = startrij; getal <= startrij+9; getal++) {
+                System.out.print(" ");
+                System.out.print(getal);
+            }
+        }
+    }
 }
+
